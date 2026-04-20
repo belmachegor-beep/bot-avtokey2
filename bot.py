2dacef43e2baa8636f63af87acbc946ddc835330
belmachegor-beep <belmachegor@gmail.com>
2026-04-20 17:52:35 +1000

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import telebot
-
-TOKEN = "8759599601:AAGOQ0h3UJkz14Qur_Gm2oimport telebot
 from telebot import types
 
 TOKEN = "8759599601:AAGOQ0h3UJkz14Qur_Gm2oTaSvEAzaOTerk"
@@ -22,7 +20,7 @@ def start(message):
     markup.add(btn_start)
 
     text = """
-🚗 <b>Аварийная замочная помощь Хабаровск</b>
+🚗 <b>Аварийная замочная помощь</b>
 
 🔓 Вскрытие авто  
 🔑 Дубликаты ключей  
@@ -155,19 +153,3 @@ def send_request(data, link, message):
 
 # запуск
 bot.infinity_polling(timeout=30, long_polling_timeout=30)
-
-bot = telebot.TeleBot(TOKEN)
-
-print("БОТ СТАРТАНУЛ")
-
-bot.delete_webhook()
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    print("ПРИШЕЛ /start")
-    bot.send_message(message.chat.id, "БОТ РАБОТАЕТ ✅")
-
-@bot.message_handler(func=lambda message: True)
-def test(message):
-    print("ПРИШЛО СООБЩЕНИЕ:", message.text)
-   
